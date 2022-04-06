aa7349e8a33185497c16d5dd006775f7451b965c
Burak Örkmez <burakorkmezz@gmail.com>
2022-04-06 21:02:39 +0300

    Filtering messages handled

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const notificationCount = document.querySelector(
 
 const messageNotifications = document.querySelector('#messages-notification');
 const messagesBox = document.querySelector('.messages');
+const message = document.querySelectorAll('.message');
+const messageSearchInput = document.querySelector('#message-search');
 
 // remove active class from all menu items
 const removeActiveClassFromMenuItems = () => {
@@ -30,7 +32,7 @@ menuItems.forEach((item) => {
 	});
 });
 
-// MESSAGES //
+// Highlight message box when sidebar message menu item is clicked //
 messageNotifications.addEventListener('click', () => {
 	messageNotifications.querySelector('.notification-count').style.display =
 		'none';
@@ -39,3 +41,18 @@ messageNotifications.addEventListener('click', () => {
 		messagesBox.style.boxShadow = 'none';
 	}, 2000);
 });
+
+const searchMessage = (e) => {
+	const val = messageSearchInput.value.toLowerCase();
+	message.forEach((msg) => {
+		let name = msg.querySelector('h5').textContent.toLowerCase();
+		if (name.includes(val)) {
+			msg.style.display = 'flex';
+		} else {
+			msg.style.display = 'none';
+		}
+	});
+};
+
+// Filter messages //
+messageSearchInput.addEventListener('keyup', searchMessage);
